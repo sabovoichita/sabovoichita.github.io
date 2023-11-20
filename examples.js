975601c783c42ec1f0fe63ad1a2012c4cd6c59dd
voichita <sabo_voichita@yahoo.com>
2023-11-20 19:42:06 +0100

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,15 @@
+console.info(2 + 3);
+console.warn("voichita");
+var employed = true;
+console.log("Angajat:", employed);
+employed = false;
+console.log("Angajat:", employed);
+employed = "da";
+console.log("Angajat:", employed);
+
+var skills = ["html", "css"];
+
+console.debug("tipul variabilei skills:", typeof skills);
+var job = document.getElementById("job-title");
+console.warn("job", job);
+console.info(typeof job);
