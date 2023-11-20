9619e1da20d66d8a4994fd388fc60181758c000d
voichita <sabo_voichita@yahoo.com>
2023-11-20 20:18:55 +0100

    update  with JS

--- examples.js
+++ examples.js
@@ -1,15 +1,23 @@
-console.info(2 + 3);
-console.warn("voichita");
-var employed = true;
-console.log("Angajat:", employed);
-employed = false;
-console.log("Angajat:", employed);
-employed = "da";
-console.log("Angajat:", employed);
+function variableExamples() {
+  console.info(2 + 3);
+  console.warn("voichita");
+  var employed = true;
+  console.log("Angajat:", employed);
+  employed = false;
+  console.log("Angajat:", employed);
+  employed = "da";
+  console.log("Angajat:", employed);
 
-var skills = ["html", "css"];
+  var skills = ["html", "css"];
+  console.debug("tipul variabilei skills:", typeof skills);
+}
 
-console.debug("tipul variabilei skills:", typeof skills);
-var job = document.getElementById("job-title");
-console.warn("job", job);
-console.info(typeof job);
+function updateTitle(title) {
+  var job = document.getElementById("job-title");
+  console.warn("job", job);
+  console.info(typeof job);
+  job.innerHTML = title;
+}
+variableExamples();
+
+updateTitle("Developer");
