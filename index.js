521a3069078110b616ffe834ff1745adec9d797b
voichita <sabo_voichita@yahoo.com>
2023-11-27 20:14:40 +0100

    hide Old page

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 function showHome() {
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
   var page = document.getElementById("home");
   page.style.display = "block";
 }
-showHome();
+// showHome();
 
 function showSkills() {
   var page = document.getElementById("skills");
   page.style.display = "block";
 }
-showSkills();
+// showSkills();
 
 function showProjects() {
   var page = document.getElementById("projects");
   page.style.display = "block";
 }
-showProjects();
+// showProjects();
 
 function showLanguages() {
   var page = document.getElementById("languages");
   page.style.display = "block";
 }
-showLanguages();
+// showLanguages();
