87a1eebb20f7948425cd2fcbb1c00a0ba7659a81
voichita <sabo_voichita@yahoo.com>
2023-11-27 20:10:33 +0100

    Add onclick on menu bar links and modify index.js and add functions to show all menu

---
+++ index.js
@@ -0,0 +1,23 @@
+function showHome() {
+  var page = document.getElementById("home");
+  page.style.display = "block";
+}
+showHome();
+
+function showSkills() {
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+showSkills();
+
+function showProjects() {
+  var page = document.getElementById("projects");
+  page.style.display = "block";
+}
+showProjects();
+
+function showLanguages() {
+  var page = document.getElementById("languages");
+  page.style.display = "block";
+}
+showLanguages();
