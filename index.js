4d8c2adb7ac269c5d2b6d0825ec34f0df223f127
Voichita Maria <sabo_voichita@yahoo.com>
2023-12-12 16:16:26 +0100

    add DARK mode

--- index.js
+++ index.js
@@ -33,3 +33,27 @@ function initEvents() {
 //Execute on start
 showPage(activePage);
 initEvents();
+
+// <-- /////////////////////////     TOGGLE BTN DARK MODE START     /////////////////////////// --> //
+
+const body = document.querySelector("body");
+const toggleBtn = document.querySelector(".toggle-container");
+const theme = localStorage.getItem("theme");
+
+theme && body.classList.add(theme);
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark-mode");
+  } else {
+    localStorage.removeItem("theme");
+  }
+});
+
+toggleBtn.addEventListener("click", () =>
+  toggleBtn.classList.toggle("toggle_active")
+);
+////////////////////////////////////////////   Dark Mode by Matei  ///////////////////////////////
+document.querySelector(".toggle-container").addEventListener("click", () => {
+  document.body.classList.toggle("grayscale");
+});
