5d9ac92b1105e4794693753a21c82722cde1cb74
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-03 22:25:53 +0100

    css for mobile

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// global variables
 let activePage = "home";
 
-// functions
 function $(selector) {
   const el = document.querySelector(selector);
-  // console.info("%o found: ", selector, el);
   return el;
 }
+
 function hide(id) {
-  console.info("hide", id);
   $("#" + id).style.display = "none";
   document.getElementById(id).style.display = "none";
 }
 
 function show(id) {
-  console.info("show", id);
   var page = $(`#${id}`);
-  console.info("page", page);
   page.style.display = "block";
 }
 
 function showPage(id) {
-  console.info("show page", id);
   const prevLink = $("a[data-page=" + activePage + "]");
   prevLink.classList.remove("active");
   hide(activePage);
@@ -37,26 +31,25 @@ function initEvents() {
   toolbar.addEventListener("click", (e) => {
     if (e.target.matches("a")) {
       const page = e.target.dataset.page;
-      console.warn("click %o", page);
       showPage(page);
     }
   });
 }
+
 function sortSkillsByEndorcements(a, b) {
-  console.info("sort", a, b);
   return b.endorcements - a.endorcements;
 }
 
 function sortByName(a, b) {
   return a.name.localeCompare(b.name);
 }
+
 function sortProjects(a, b) {
   return a.Project.localeCompare(b.Project);
 }
 
 function showSkills(skills) {
   skills.sort(sortByName);
-  // skills.sort(sortSkillsByEndorcements);
 
   const ul = $("#skills ul");
 
@@ -66,24 +59,11 @@ function showSkills(skills) {
       cls = "favorite";
     }
 
-    console.info("%o (%o)", skill.favorite, cls);
     return `<li class="${cls}">${skill.name}<span> - ${skill.endorcements}</span></li>`;
   });
-  console.warn(text);
 
   ul.innerHTML = text.join("");
 }
-// function showProjects(projects) {
-//   projects.sort(sortProjects);
-//   var ulProjects = $("#projects ul");
-
-//   var textP= projects.map(function(project){
-//     var clsP = "";
-//     if (project.Completed == true){
-//       clsP="Completed";
-//     });
-//   ulProjects.innerHTML = textP.join("");
-// }
 
 function loadSkills() {
   fetch("skills.json").then((r) => {
@@ -92,13 +72,6 @@ function loadSkills() {
     });
   });
 }
-// function loadProjects() {
-//   fetch("projects.json").then(function (r) {
-//     r.json().then(function (skills) {
-//       showProjects(projects);
-//     });
-//   });
-// }
 
 function loadLanguages() {
   fetch("languages.json")
@@ -118,16 +91,13 @@ function printJsonIntoTable(jsonData, elementId) {
     }
   }
 
-  //This Code creates HTML table
   var table = document.createElement("table");
-  //This Code gets rows for header creader above.
   var tr = table.insertRow(-1);
   for (var i = 0; i < col.length; i++) {
     var th = document.createElement("th");
     th.innerHTML = col[i];
     tr.appendChild(th);
   }
-  //This Code adds data to table as rows
   for (var i = 0; i < jsonData.length; i++) {
     tr = table.insertRow(-1);
 
@@ -136,27 +106,11 @@ function printJsonIntoTable(jsonData, elementId) {
       tabCell.innerHTML = jsonData[i][col[j]];
     }
   }
-  //This Code gets the all columns for header
   var divContainer = document.getElementById(elementId);
   divContainer.innerHTML = " ";
   divContainer.appendChild(table);
 }
-//Execute on start
-showPage(activePage);
-initEvents();
-loadSkills();
-loadLanguages();
-// loadProjects();
 
-// function showRubik() {
-//   var rubik = $("rubik div");
-//   var cube = ["red", "green", "blue"].map(function () {
-//     return "<div></div>";
-//     rubik.innerHTML = cube;
-//   });
-// }
-
-console.warn("after");
 function rubik() {
   const div1 = document.createElement("div");
   $("#rubik div").appendChild(div1);
@@ -185,34 +139,6 @@ function rubik() {
   const div9 = document.createElement("div");
   $("#rubik div").appendChild(div9);
 }
-rubik();
-
-// Function to Create a table with JS
-
-// function rubikTable() {
-//   const tbl = document.createElement("table");
-//   $("#rubik div").appendChild(tbl);
-//   const tblBody = document.createElement("tbody");
-//   $("#rubik div").appendChild(tblBody);
-//   for (let i = 0; i < 3; i++) {
-//     const row = document.createElement("tr");
-//     for (let j = 0; j < 3; j++) {
-//       const cell = document.createElement("td");
-//       row.appendChild(cell);
-//     }
-//     tblBody.appendChild(row);
-//   }
-//   tbl.appendChild(tblBody);
-//   tbl.setAttribute("border", "2");
-// }
-// rubikTable();
-
-// function myFunction() {
-//   var x = document.getElementById("rubik-face").rows[0].cells;
-//   x[0].innerHTML = "NEW CONTENT";
-// }
-
-// <-- /////////////////////////     TOGGLE BTN DARK MODE START     /////////////////////////// --> //
 
 const body = document.querySelector("body");
 const toggleBtn = document.querySelector(".toggle-container");
@@ -231,7 +157,12 @@ toggleBtn.addEventListener("click", () => {
 toggleBtn.addEventListener("click", () =>
   toggleBtn.classList.toggle("toggle_active")
 );
-////////////////////////////////////////////   Dark Mode by Matei  ///////////////////////////////
 document.querySelector(".toggle-container").addEventListener("click", () => {
   document.body.classList.toggle("grayscale");
 });
+
+showPage(activePage);
+initEvents();
+loadSkills();
+loadLanguages();
+rubik();
